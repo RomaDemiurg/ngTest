261fc66db1d7f3981242b6433f07ed949b3efbad
RomaDemiurg <demiurgru@yandex.ru>
2017-09-19 01:07:39 +0300

    get feeds from firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  currentUser: firebase.User;
+  feeds: any;
+
+  constructor(private zone: NgZone) {
+  }
+
+  ngOnInit(): void {
+    firebase.database().ref('feeds').limitToFirst(3).on('value', (snap) => {
+      this.feeds = snap.toJSON();
+      console.log(this.feeds);
+      this.zone.run(() => {
+      });
+    });
+
+    this.currentUser = firebase.auth().currentUser;
+    console.log('currentUser: ' + this.currentUser);
+  }
+
+  getAuthor() {
+    console.log('feeds: ' + Object.keys(this.feeds).map((key) => this.feeds[key].author));
+  }
 }
