276ca391f08bc291f03c84a570df120bdf1b10f2
RomaDemiurg <demiurgru@yandex.ru>
2017-09-19 02:28:29 +0300

    reformat again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component, NgZone, OnInit} from '@angular/core';
-import * as firebase from 'firebase';
+import { Component, NgZone, OnInit } from '@angular/core'
+import * as firebase from 'firebase'
 
 
 @Component({
@@ -8,23 +8,23 @@ import * as firebase from 'firebase';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  currentUser: firebase.User;
-  feeds: any;
+  currentUser: firebase.User
+  feeds: any
 
   constructor(private zone: NgZone) { }
 
   ngOnInit(): void {
     firebase.database().ref('feeds').limitToFirst(3).on('value', (snap) => {
-      this.feeds = snap.toJSON();
-      console.log(this.feeds);
-      this.zone.run(() => {});
-    });
+      this.feeds = snap.toJSON()
+      console.log(this.feeds)
+      this.zone.run(() => {})
+    })
 
-    this.currentUser = firebase.auth().currentUser;
-    console.log('currentUser: ' + this.currentUser);
+    this.currentUser = firebase.auth().currentUser
+    console.log('currentUser: ' + this.currentUser)
   }
 
   getAuthor() {
-    console.log('feeds: ' + Object.keys(this.feeds).map((key) => this.feeds[key].author));
+    console.log('feeds: ' + Object.keys(this.feeds).map((key) => this.feeds[key].author))
   }
 }
