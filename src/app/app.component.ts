b63d8c3806dc81fdb6537bb87441e5b81f0adbad
RomaDemiurg <demiurgru@yandex.ru>
2017-09-19 02:48:10 +0300

    indent with 4 spaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,28 +3,28 @@ import * as firebase from 'firebase'
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  currentUser: firebase.User
-  feeds: any
+    currentUser: firebase.User
+    feeds: any
 
-  constructor(private zone: NgZone) { }
+    constructor(private zone: NgZone) { }
 
-  ngOnInit(): void {
-    firebase.database().ref('feeds').limitToFirst(3).on('value', (snap) => {
-      this.feeds = snap.toJSON()
-      console.log(this.feeds)
-      this.zone.run(() => {})
-    })
+    ngOnInit(): void {
+        firebase.database().ref('feeds').limitToFirst(3).on('value', (snap) => {
+            this.feeds = snap.toJSON()
+            console.log(this.feeds)
+            this.zone.run(() => {})
+        })
 
-    this.currentUser = firebase.auth().currentUser
-    console.log('currentUser: ' + this.currentUser)
-  }
+        this.currentUser = firebase.auth().currentUser
+        console.log('currentUser: ' + this.currentUser)
+    }
 
-  getAuthor() {
-    console.log('feeds: ' + Object.keys(this.feeds).map((key) => this.feeds[key].author))
-  }
+    getAuthor() {
+        console.log('feeds: ' + Object.keys(this.feeds).map((key) => this.feeds[key].author))
+    }
 }
