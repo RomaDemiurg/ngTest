d630a4749012d51aef6880aef3b6d3f3e3dd9f08
RomaDemiurg <demiurgru@yandex.ru>
2017-09-19 02:26:09 +0300

    adjust formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, NgZone, OnInit} from '@angular/core';
 import * as firebase from 'firebase';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,15 +11,13 @@ export class AppComponent implements OnInit {
   currentUser: firebase.User;
   feeds: any;
 
-  constructor(private zone: NgZone) {
-  }
+  constructor(private zone: NgZone) { }
 
   ngOnInit(): void {
     firebase.database().ref('feeds').limitToFirst(3).on('value', (snap) => {
       this.feeds = snap.toJSON();
       console.log(this.feeds);
-      this.zone.run(() => {
-      });
+      this.zone.run(() => {});
     });
 
     this.currentUser = firebase.auth().currentUser;
