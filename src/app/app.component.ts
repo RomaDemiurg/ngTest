3f09ccd380ab11e47c8a10985dff59c11fdadf0e
RomaDemiurg <demiurgru@yandex.ru>
2017-09-19 18:16:54 +0300

    login logout getAuthState

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,28 +3,57 @@ import * as firebase from 'firebase'
 
 
 @Component({
-    selector: 'app-root',
+    selector   : 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls  : ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    currentUser: firebase.User
+    currentUser: firebase.UserInfo
     feeds: any
 
     constructor(private zone: NgZone) { }
 
     ngOnInit(): void {
+        this.getFeeds()
+        this.getAuthState()
+    }
+
+    getAuthState() {
+        firebase.auth().onAuthStateChanged(
+            (user) => {
+                this.currentUser = user
+                if ( this.currentUser )
+                    console.log('onAuthStateChanged: LoggedIn', this.currentUser)
+                else
+                    console.log('onAuthStateChanged: LoggedOut')
+            },
+            (error) => {
+                console.log('Error in onAuthStateChanged:', error)
+            }
+        )
+    }
+
+    logOut() {
+        firebase.auth().signOut()
+        console.log('Logged Out:')
+    }
+
+    logIn() {
+        firebase.auth().signInWithEmailAndPassword('qwerty@mail.ru', '123456').then(
+            (user: firebase.UserInfo) => {
+                console.log('Success in logIn:', user.displayName)
+            },
+            (error) => {
+                console.log('Error in logIn:', error)
+            }
+        )
+    }
+
+    getFeeds() {
         firebase.database().ref('feeds').limitToFirst(3).on('value', (snap) => {
             this.feeds = snap.toJSON()
             console.log(this.feeds)
             this.zone.run(() => {})
         })
-
-        this.currentUser = firebase.auth().currentUser
-        console.log('currentUser: ' + this.currentUser)
-    }
-
-    getAuthor() {
-        console.log('feeds: ' + Object.keys(this.feeds).map((key) => this.feeds[key].author))
     }
 }
