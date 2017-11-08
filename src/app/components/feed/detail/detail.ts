9fbf6cb657be6cdc0114994b96f9f87f9dfa14df
RomaDemiurg <demiurgru@yandex.ru>
2017-11-08 19:12:21 +0200

    remove unnecessary code

--- src/app/components/feed/detail/detail.ts
+++ src/app/components/feed/detail/detail.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core'
-import * as firebase from 'firebase'
 
 @Component({
     selector: 'app-feed-detail',
@@ -7,25 +6,4 @@ import * as firebase from 'firebase'
 })
 export class FeedDetailComponent {
     @Input() feed
-    // feedRef: firebase.firestore.DocumentReference
-
-    /* updateLikes(feed) {
-        this.feedRef = firebase.firestore().collection('feeds').doc(feed.id)
-
-        this.feedRef.update({likes: feed.likes + 1})
-    } */
-
-    /* deleteFeed(feedId) {
-        this.feedRef = firebase.firestore().collection('feeds').doc(feedId)
-
-        this.feedRef.delete().then(
-            success => {
-                console.log('Success: deleteFeed()')
-            },
-            error => {
-                console.log('Error: deleteFeed()')
-            }
-        )
-    } */
-
 }
