31714bca5f42d0010fc5af6f3fc67476442d0f28
RomaDemiurg <demiurgru@yandex.ru>
2017-11-08 18:47:19 +0200

    add method deleteFeed(feedId)

--- src/app/components/feed/feed.service.ts
+++ src/app/components/feed/feed.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
+import * as firebase from 'firebase'
 
 @Injectable()
 export class FeedService {
+    feedRef: firebase.firestore.DocumentReference
 
-  constructor() { }
+    deleteFeed(feedId) {
+        this.feedRef = firebase.firestore().collection('feeds').doc(feedId)
+
+        this.feedRef.delete().then(
+            success => {
+                console.log('Success: deleteFeed()')
+            },
+            error => {
+                console.log('Error: deleteFeed()')
+            }
+        )
+    }
 
 }
