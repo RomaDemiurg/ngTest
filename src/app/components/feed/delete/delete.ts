0868da7de0f325c53b946bf9084615bea18c340e
RomaDemiurg <demiurgru@yandex.ru>
2017-11-08 18:44:54 +0200

    refactor deleteFeed() to feedService.deleteFeed()

--- src/app/components/feed/delete/delete.ts
+++ src/app/components/feed/delete/delete.ts
@@ -1,24 +1,12 @@
 import { Component, Input } from '@angular/core'
-import * as firebase from 'firebase'
+import { FeedService } from '../feed.service'
 
 @Component({
-  selector: 'app-feed-delete',
-  templateUrl: './delete.html'
+    selector   : 'app-feed-delete',
+    templateUrl: './delete.html'
 })
 export class FeedDeleteComponent {
     @Input() feedId
-    feedRef: firebase.firestore.DocumentReference
 
-    deleteFeed() {
-        this.feedRef = firebase.firestore().collection('feeds').doc(this.feedId)
-
-        this.feedRef.delete().then(
-            success => {
-                console.log('Success: deleteFeed()')
-            },
-            error => {
-                console.log('Error: deleteFeed()')
-            }
-        )
-    }
+    constructor(protected feedService: FeedService) { }
 }
