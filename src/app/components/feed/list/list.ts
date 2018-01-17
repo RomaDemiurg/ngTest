2d75eb100c25e4a93bdf1057809b3e5d23a58b6d
RomaDemiurg <demiurgru@yandex.ru>
2018-01-17 19:39:07 +0200

    Implement createComment()

--- src/app/components/feed/list/list.ts
+++ src/app/components/feed/list/list.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core'
 import { FeedService } from '../feed.service'
+import * as firebase from 'firebase'
+
+interface Comment {
+    id?: string
+    author: string
+    img: string
+    likes: number | string
+    text: string
+    created_at: Date
+}
 
 @Component({
     selector: 'app-feeds-list',
@@ -9,6 +19,21 @@ export class FeedsListComponent {
 
     constructor(public feedService: FeedService) {
         this.feedService.getNextFeeds()
+    }
+
+    createComment(feedId, author, img, likes, text) {
+        const comment: Comment = {
+            author: author,
+            img: img,
+            likes: likes,
+            text: text,
+            created_at: new Date()
+        }
+
+        /*firebase.database().ref('comments/' + this.feedKey).push(comment)*/
+
+        // feeds/MO6bD7As3gO2KPvGw2Sa/comments/h3aPBrJQQacOeAbYvqgp
+        firebase.firestore().collection('feeds/' + feedId + '/comments').add(comment)
     }
 
 }
